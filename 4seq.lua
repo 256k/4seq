038918043603c298631dcea98f34e6a32c70fa4b
256k <fayek.helmi@gmail.com>
2025-01-04 02:22:29 +0000

    mvp complete

---
+++ 4seq.lua
@@ -0,0 +1,89 @@
+-- 4seq
+-- arc based 4x prolly sequencer
+--
+-- by 256k
+engine.name = 'PolyPerc'
+
+local arc4 = arc.connect()
+-- CONSTANTS
+local PROB_THRESHOLD_LIMIT = 101
+-- ---------
+local problist = {}
+local thold = {}
+local playhead = {}
+
+function tholdInit(tholdVal)
+  for t=1,4 do
+    thold[t] = tholdVal
+  end
+end
+
+function playheadInit()
+  for p=1,4 do
+    playhead[p] = 1
+  end
+end
+
+function playheadInc(playheadNum, seqLength)
+  local ph = playhead[playheadNum]
+  if ph >= seqLength then 
+    playhead[playheadNum] = 1 
+    return
+  end
+  playhead[playheadNum] = ph + 1
+end
+
+
+function problistInit(thld_limit)
+  for a=1,4 do
+    problist[a] = {}  
+    for i=1,64 do
+      problist[a][i] = math.random(thld_limit)
+    end
+  end
+end
+
+function init()
+  problistInit(PROB_THRESHOLD_LIMIT)
+  tholdInit(PROB_THRESHOLD_LIMIT)
+  playheadInit()
+  
+  for i=1,4 do
+  drawRingProbSeq(i)
+  end
+  
+  clock.run(function()
+    while true do
+      clock.sync(1/4)
+      for i=1,4 do
+        playheadInc(i, 64)
+        drawRingProbSeq(i)
+        -- print(problist[i][playhead[i]])
+        if problist[i][playhead[i]] > thold[i] then
+        engine.hz(220 * i)
+        end
+
+  end
+    end
+  end)
+end
+
+
+function arc.delta(n,d)
+  print("THOLD: ", thold[n])
+  thold[n] = util.clamp((thold[n] + d / 16 ), 0, PROB_THRESHOLD_LIMIT )
+  drawRingProbSeq(n)
+end
+
+function drawRingProbSeq(ring)
+for i=1,64 do
+  local ledVal
+  if problist[ring][i] > thold[ring] then
+    ledVal = 10
+    else ledVal = 0
+  end
+  if i == playhead[ring] then ledVal = 15 end
+  arc4:led(ring, i, ledVal )
+end
+arc4:refresh()
+end
